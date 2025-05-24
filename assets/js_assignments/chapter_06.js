86faa6976ef80d7946e245066121d3205839bb6e
Umair Zahid <umairzahid7744@gmail.com>
2025-05-24 17:56:21 +0500

    chapter 6

---
+++ assets/js_assignments/chapter_06.js
@@ -0,0 +1,40 @@
+// task 1
+
+var num1 = +prompt("Enter your number: ")
+document.write ("<h2>Result:</h2>")
+document.write ("The Value of a is: " + num1)
+document.write ("<br>.......................")
+document.write ("<br><br> The Value of ++a is: " + ++num1)
+document.write ("<br>Now the Value of a is: " + num1)
+document.write ("<br><br> The Value of a++ is: " + num1++)
+document.write ("<br>Now the Value of a is: " + num1)
+document.write ("<br><br> The Value of --a is: " + --num1)
+document.write ("<br>Now the Value of a is: " + num1)
+document.write ("<br><br> The Value of a-- is: " + num1--)
+document.write ("<br>Now the Value of a is: " + num1)
+
+// task 2
+
+var a = num1
+var b = +prompt("Enter your second number:")
+document.write ("<br><br>The Value of a is: " + a)
+document.write ("<br>The Value of b is: " + b)
+document.write ("<h2>Result:</h2>")
+document.write ("--a = " + --a)
+document.write ("<br>--b = " + --b)
+document.write ("<br>--a - --b = " + (--a - --b))
+document.write ("<br>--a - --b + ++b = " + ((--a - --b) + ++b))
+document.write ("<br>--a - --b + ++b + b-- = " + (((--a - --b) + ++b) + b--))
+
+// task 3
+
+var name = prompt("Enter your name:")
+document.write ("<h2>Welcome " + name + " !Have a nice day!</h2>");
+
+// task 4
+
+// task 5
+
+// task 6
+
+// the end
