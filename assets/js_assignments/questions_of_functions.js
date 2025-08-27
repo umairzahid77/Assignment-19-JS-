21774ce8cfad20c4a5e4910ee12e5dce2a106057
Umair Zahid <umairzahid7744@gmail.com>
2025-08-27 20:01:30 +0500

    update

--- assets/js_assignments/questions_of_functions.js
+++ assets/js_assignments/questions_of_functions.js
@@ -1,35 +1,71 @@
 // task 01
 
 function task01() {
+    let array = []
+    let sum = 0
+    for (let i = 0; i < 5; i++) {
+        array.push(+prompt(`Enter number ${i + 1}`))
+    }
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i]
+    }
     let t01 = document.getElementById("t01")
-    t01.innerText = ``
-    t01.innerHTML = ``
+    t01.innerText = `Array entered: [${array}]
+    Sum of array: ${sum}`
 }
 
 function showtask01() {
     let show01 = document.getElementById("task01")
-    show01.innerText = ``
+    show01.innerText = `let array = []
+    let sum = 0
+    for (let i = 0; i < 5; i++) {
+        array.push(+prompt(Enter number $ {i + 1}))
+    }
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i]
+    }
+    let t01 = document.getElementById("t01")
+    t01.innerText = Array entered: [$ {array}]
+    Sum of array: $ {sum}`
 }
 
 // task 02
 
 function task02() {
+    let str = prompt("Enter your string")
     let t02 = document.getElementById("t02")
     t02.innerText = ``
-    t02.innerHTML = ``
+    for (let i = (str.length - 1); i >= 0; i--) {
+        t02.innerText += `${str[i]}`
+    }
 }
 
 function showtask02() {
     let show02 = document.getElementById("task02")
-    show02.innerText = ``
+    show02.innerText = `let str = prompt("Enter your string")
+    let t02 = document.getElementById("t02")
+    t02.innerText = ``
+    for (let i = (str.length - 1); i >= 0; i--) {
+        t02.innerText += $ {str[i]}
+    }`
 }
 
 // task 03
 
 function task03() {
+    let array
+    let big = 0
+    for (let i = 0; i < 5; i++) {
+        array.push(+prompt(`Enter number ${i + 1}`))
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (big < array[i]) {
+            big = array[i]
+        }
+    }
     let t03 = document.getElementById("t03")
-    t03.innerText = ``
-    t03.innerHTML = ``
+    t03.innerText = `Array entered: [${array}
+    Largest in array: ${big}`
 }
 
 function showtask03() {
@@ -76,188 +112,6 @@ function showtask06() {
     show06.innerText = ``
 }
 
-// task 07
-
-function task07() {
-    let t07 = document.getElementById("t07")
-    t07.innerText = ``
-    t07.innerHTML = ``
-}
-
-function showtask07() {
-    let show07 = document.getElementById("task07")
-    show07.innerText = ``
-}
-
-// task 08
-
-function task08() {
-    let t08 = document.getElementById("t08")
-    t08.innerText = ``
-    t08.innerHTML = ``
-}
-
-function showtask08() {
-    let show08 = document.getElementById("task08")
-    show08.innerText = ``
-}
-
-// task 09
-
-function task09() {
-    let t09 = document.getElementById("t09")
-    t09.innerText = ``
-    t09.innerHTML = ``
-}
-
-function showtask09() {
-    let show09 = document.getElementById("task09")
-    show09.innerText = ``
-}
-
-// task 10
-
-function task10() {
-    let t10 = document.getElementById("t10")
-    t10.innerText = ``
-    t10.innerHTML = ``
-}
-
-function showtask10() {
-    let show10 = document.getElementById("task10")
-    show10.innerText = ``
-}
-
-// task 11
-
-function task11() {
-    let t11 = document.getElementById("t11")
-    t11.innerText = ``
-    t11.innerHTML = ``
-}
-
-function showtask11() {
-    let show11 = document.getElementById("task11")
-    show11.innerText = ``
-}
-
-// task 12
-
-function task12() {
-    let t12 = document.getElementById("t12")
-    t12.innerText = ``
-    t12.innerHTML = ``
-}
-
-function showtask12() {
-    let show12 = document.getElementById("task12")
-    show12.innerText = ``
-}
-
-// task 13
-
-function task13() {
-    let t13 = document.getElementById("t13")
-    t13.innerText = ``
-    t13.innerHTML = ``
-}
-
-function showtask13() {
-    let show13 = document.getElementById("task13")
-    show13.innerText = ``
-}
-
-// task 14
-
-function task14() {
-    let t14 = document.getElementById("t14")
-    t14.innerText = ``
-    t14.innerHTML = ``
-}
-
-function showtask14() {
-    let show14 = document.getElementById("task14")
-    show14.innerText = ``
-}
-
-// task 15
-
-function task15() {
-    let t15 = document.getElementById("t15")
-    t15.innerText = ``
-    t15.innerHTML = ``
-}
-
-function showtask15() {
-    let show15 = document.getElementById("task15")
-    show15.innerText = ``
-}
-
-// task 16
-
-function task16() {
-    let t16 = document.getElementById("t16")
-    t16.innerText = ``
-    t16.innerHTML = ``
-}
-
-function showtask16() {
-    let show16 = document.getElementById("task16")
-    show16.innerText = ``
-}
-
-// task 17
-
-function task17() {
-    let t17 = document.getElementById("t17")
-    t17.innerText = ``
-    t17.innerHTML = ``
-}
-
-function showtask17() {
-    let show17 = document.getElementById("task17")
-    show17.innerText = ``
-}
-
-// task 18
-
-function task18() {
-    let t18 = document.getElementById("t18")
-    t18.innerText = ``
-    t18.innerHTML = ``
-}
-
-function showtask18() {
-    let show18 = document.getElementById("task18")
-    show18.innerText = ``
-}
-
-// task 19
-
-function task19() {
-    let t19 = document.getElementById("t19")
-    t19.innerText = ``
-    t19.innerHTML = ``
-}
-
-function showtask19() {
-    let show19 = document.getElementById("task19")
-    show19.innerText = ``
-}
-
-// task 20
-
-function task20() {
-    let t20 = document.getElementById("t20")
-    t20.innerText = ``
-    t20.innerHTML = ``
-}
-
-function showtask20() {
-    let show20 = document.getElementById("task20")
-    show20.innerText = ``
-}
-
 // clear all
 
 function clearall() {
@@ -279,48 +133,6 @@ function clearall() {
     let t06 = document.getElementById("t06")
     t06.innerText = ``
     t06.innerHTML = ``
-    let t07 = document.getElementById("t07")
-    t07.innerText = ``
-    t07.innerHTML = ``
-    let t08 = document.getElementById("t08")
-    t08.innerText = ``
-    t08.innerHTML = ``
-    let t09 = document.getElementById("t09")
-    t09.innerText = ``
-    t09.innerHTML = ``
-    let t10 = document.getElementById("t10")
-    t10.innerText = ``
-    t10.innerHTML = ``
-    let t11 = document.getElementById("t11")
-    t11.innerText = ``
-    t11.innerHTML = ``
-    let t12 = document.getElementById("t12")
-    t12.innerText = ``
-    t12.innerHTML = ``
-    let t13 = document.getElementById("t13")
-    t13.innerText = ``
-    t13.innerHTML = ``
-    let t14 = document.getElementById("t14")
-    t14.innerText = ``
-    t14.innerHTML = ``
-    let t15 = document.getElementById("t15")
-    t15.innerText = ``
-    t15.innerHTML = ``
-    let t16 = document.getElementById("t16")
-    t16.innerText = ``
-    t16.innerHTML = ``
-    let t17 = document.getElementById("t17")
-    t17.innerText = ``
-    t17.innerHTML = ``
-    let t18 = document.getElementById("t18")
-    t18.innerText = ``
-    t18.innerHTML = ``
-    let t19 = document.getElementById("t19")
-    t19.innerText = ``
-    t19.innerHTML = ``
-    let t20 = document.getElementById("t20")
-    t20.innerText = ``
-    t20.innerHTML = ``
     let show01 = document.getElementById("task01")
     show01.innerText = ``
     let show02 = document.getElementById("task02")
@@ -333,34 +145,6 @@ function clearall() {
     show05.innerText = ``
     let show06 = document.getElementById("task06")
     show06.innerText = ``
-    let show07 = document.getElementById("task07")
-    show07.innerText = ``
-    let show08 = document.getElementById("task08")
-    show08.innerText = ``
-    let show09 = document.getElementById("task09")
-    show09.innerText = ``
-    let show10 = document.getElementById("task10")
-    show10.innerText = ``
-    let show11 = document.getElementById("task11")
-    show11.innerText = ``
-    let show12 = document.getElementById("task12")
-    show12.innerText = ``
-    let show13 = document.getElementById("task13")
-    show13.innerText = ``
-    let show14 = document.getElementById("task14")
-    show14.innerText = ``
-    let show15 = document.getElementById("task15")
-    show15.innerText = ``
-    let show16 = document.getElementById("task16")
-    show16.innerText = ``
-    let show17 = document.getElementById("task17")
-    show17.innerText = ``
-    let show18 = document.getElementById("task18")
-    show18.innerText = ``
-    let show19 = document.getElementById("task19")
-    show19.innerText = ``
-    let show20 = document.getElementById("task20")
-    show20.innerText = ``
 }
 
 // the end
